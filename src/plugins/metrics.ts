73b93429cb4fe45558bac38aee70ba49aa0675f9
“Bugs5382” <bugs5382@users.noreply.github.com>
2024-03-20 10:55:31 -0400

    feat: unit testing [ci skip]

---
+++ src/plugins/metrics.ts
@@ -0,0 +1,9 @@
+import fp from 'fastify-plugin'
+import metricsPlugin, {IMetricsPluginOptions} from 'fastify-metrics'
+export default fp<IMetricsPluginOptions>(async (fastify) => {
+  void fastify.register(metricsPlugin, { endpoint: '/metrics' })
+
+  void fastify.ready().then(() => {
+    fastify.log.debug('[node-microservice-itil-metrics] Started Metrics')
+  })
+})
